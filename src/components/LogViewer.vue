0111944a2b1b75877542cd500622e4413662ec14
ihgs <hgs.dev@gmail.com>
2017-12-24 16:46:30 +0900

    Drog & drop

---
+++ src/components/LogViewer.vue
@@ -0,0 +1,36 @@
+<template>
+
+<div id="upload_section">
+    <div id="drop" draggable=true style="height:50px; padding:10px; border:3px solid" v-on:dragover="onDragOver" v-on:drop="onDrop">
+        ファイルをドラッグアンドドロップしてください。複数ファイル同時も対応しています。
+    </div>
+    <ul>
+      <li v-for="item in logFiles">
+        {{item.name}}
+      </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'LogViewer',
+  data () {
+    return {
+      logFiles: []
+    }
+  },
+  methods: {
+    onDragOver: function (event) {
+      event.preventDefault()
+    },
+    onDrop: function (event) {
+      event.preventDefault()
+      var files = event.dataTransfer.files
+      for (var i = 0; i < files.length; i++) {
+        this.logFiles.push(files[i])
+      }
+    }
+  }
+}
+</script>
