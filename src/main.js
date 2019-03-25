0df90585085eb57449dd68117e2ce02cc5b08ce0
ihgs <hgs.dev@gmail.com>
2019-03-25 21:57:56 +0900

    use vue-cli v3

--- src/main.js
+++ src/main.js
@@ -11,10 +11,7 @@ Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App)
+}).$mount('#app')
